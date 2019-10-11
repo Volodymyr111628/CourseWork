99d0479542adb8d78fcadbd39f0544ec600abbcf
Volodymyr Kuhivchak <vova111628@gmail.com>
2019-10-11 18:35:30 +0300

    Add Course task3

---
+++ CoursesTask1/CoursesTask3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoursesTask3.Tasks;
+using Classes.Common.Runner;
+
+namespace CoursesTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRunnable> tasks = new List<IRunnable>()
+            {
+                new Task1()
+            };
+            foreach (var task in tasks)
+            {
+                task.Run();
+            }
+        }
+    }
+}
---
+++ CoursesTask1/CoursesTask3/Tasks/Task1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes.Common.Runner;
+using System.IO;
+using Classes.Common.Printer;
+using CoursesTask3.Common;
+
+namespace CoursesTask3.Tasks
+{
+    class Task1 : IRunnable
+    {
+        private readonly IPrinter _printer;
+
+        public Task1()
+        {
+            _printer = new ConsolePrinter();
+        }
+
+        public void Run()
+        {
+            string directoryName = "C:\\Users\\vova1\\Videos\\Desktop\\Selfimprovement";
+            Functions.GetDirectories(directoryName);
+        }
+    }
+}
